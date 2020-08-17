969e718886666a1ec93c094823c1fd25aec13bb3
sreeksp <sreekanthpalaparty30@gmail.com>
2020-08-17 00:40:10 -0700

    find min in rotated sorted array

---
+++ Arrays/minimumInRotatedSortedArray.js
@@ -0,0 +1,15 @@
+function findMin(nums) {
+    let l = 0;
+    let r = nums.length - 1;
+    while (l < r) {
+      const m = Math.floor((l + r) / 2);
+      if (nums[m] > nums[r]) {
+          l = m + 1;
+      } else{
+        r = m;
+      } 
+    }
+    return nums[l];
+  }
+
+  findMin([3,4,5,1,2])
